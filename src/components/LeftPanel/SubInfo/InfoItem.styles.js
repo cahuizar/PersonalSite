bc0e644e51b7a0de8bdfc25574bb1f3c0fbfd54f
= <hire@carloshuizar.com>
2019-12-15 15:28:17 -0500

    Add font awesome icons to each sub info row

---
+++ src/components/LeftPanel/SubInfo/InfoItem.styles.js
@@ -0,0 +1,13 @@
+import { css } from '@emotion/core';
+
+const infoItemStyles = css`
+  margin-bottom: 1em;
+  margin-top: 1em;
+  .icon {
+    color: #b2b5bf;
+    margin-right: 8px;
+    vertical-align: middle;
+  }
+`;
+
+export default infoItemStyles;
