3a0d72dd5ebc231d4307d483ee4b8c2bf0766bbf
= <hire@carloshuizar.com>
2019-12-15 15:24:40 -0500

    Remove routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import 'normalize.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Global } from '@emotion/core';
 
 import globalStyles from './App.styles';
 import { Home } from './components/pages/Home';
-import { Error } from './components/pages/Error';
 
 function App() {
   return (
-    <Router>
+    <Fragment>
       <Global styles={globalStyles} />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route component={Error} />
-      </Switch>
-    </Router>
+      <Home />
+    </Fragment>
   );
 }
 
