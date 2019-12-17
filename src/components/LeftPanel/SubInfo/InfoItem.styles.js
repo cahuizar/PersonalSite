0374f67a403abd3e98157feb121bfca238dbdd5d
Carlos Huizar <carlos.huizar@navient.com>
2019-12-17 11:27:11 -0500

    Add bottom border to the InfoItem

--- src/components/LeftPanel/SubInfo/InfoItem.styles.js
+++ src/components/LeftPanel/SubInfo/InfoItem.styles.js
@@ -1,8 +1,13 @@
 import { css } from '@emotion/core';
 
 const infoItemStyles = css`
-  margin-bottom: 1em;
   margin-top: 1em;
+  padding-bottom: 1em;
+  padding-left: 10px;
+  padding-right: 10px;
+  &:not(:last-of-type) {
+    border-bottom: 1px solid #ececff;
+  }
   .icon {
     color: #b2b5bf;
     margin-right: 8px;
