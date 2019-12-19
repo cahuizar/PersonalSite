63861b298f48d2f1e354aef574d01b0602bdf264
Carlos Huizar <carlos.huizar@navient.com>
2019-12-19 10:48:00 -0500

    address prop warning

--- src/components/Grid/Row.styles.js
+++ src/components/Grid/Row.styles.js
@@ -1,11 +1,11 @@
 import { css } from '@emotion/core';
 
-const rowStyles = css`
+const rowStyles = props => css`
   display: flex;
   flex-wrap: wrap;
   margin-right: -15px;
   margin-left: -15px;
-  justify-content: ${props => (props.justify ? props.justify : undefined)};
+  justify-content: ${props.justify ? props.justify : undefined};
 `;
 
 export default rowStyles;
