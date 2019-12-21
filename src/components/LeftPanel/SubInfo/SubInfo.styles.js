c78d6dce560f6ab016abacf6dcf687baff8891f4
= <hire@carloshuizar.com>
2019-12-21 11:13:52 -0500

    Add social icons

--- src/components/LeftPanel/SubInfo/SubInfo.styles.js
+++ src/components/LeftPanel/SubInfo/SubInfo.styles.js
@@ -4,6 +4,24 @@ const subInfoStyles = css`
   display: flex;
   justify-content: center;
   margin: 2rem auto;
+  .social {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-top: 1rem;
+    a {
+      color: #b2b5bf;
+      &:hover,
+      &:active,
+      &:focus {
+        color: #538fff;
+      }
+    }
+    .icon {
+      font-size: 1.7rem;
+      vertical-align: text-top;
+    }
+  }
 `;
 
 export default subInfoStyles;
