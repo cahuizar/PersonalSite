7a477b2d802f3fe38bb1ba8823add97e0d782c09
Carlos Huizar <carlos.huizar@navient.com>
2019-12-22 15:18:46 -0500

    Add styles to handle smaller col sizes

--- src/components/Grid/Col.styles.js
+++ src/components/Grid/Col.styles.js
@@ -1,21 +1,35 @@
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 
-export const xsFullStyles = css`
+const col4 = css`
+  @media (min-width: 576px) {
+    flex: 0 0 33.3333333%;
+    max-width: 33.3333333%;
+  }
+`;
+
+const col6 = css`
+  @media (min-width: 576px) {
+    flex: 0 0 58.333333%;
+    max-width: 58.333333%;
+  }
+`;
+
+const ColStyles = styled.div`
   position: relative;
   width: 100%;
   padding-right: 15px;
   padding-left: 15px;
   flex: 0 0 100%;
   max-width: 100%;
+  ${props => {
+    if (props.sm === '4') {
+      return col4;
+    } else if (props.sm === '6') {
+      return col6;
+    }
+    return undefined;
+  }}
 `;
 
-export const smHalfStyles = css`
-  @media (min-width: 576px) {
-    position: relative;
-    width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-    flex: 0 0 50%;
-    max-width: 50%;
-  }
-`;
+export default ColStyles;
