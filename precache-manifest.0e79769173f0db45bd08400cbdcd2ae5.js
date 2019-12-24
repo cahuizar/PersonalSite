fe3065ca8af66c882cbd2afaaec58854ea16b9f3
= <hire@carloshuizar.com>
2019-12-24 12:57:43 -0500

    Updates

---
+++ precache-manifest.0e79769173f0db45bd08400cbdcd2ae5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09ac3164db2e428a7150885e1f4a165",
+    "url": "/PersonalSite/index.html"
+  },
+  {
+    "revision": "d94f77d2859af6a5ac72",
+    "url": "/PersonalSite/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d94f77d2859af6a5ac72",
+    "url": "/PersonalSite/static/js/2.73d69356.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/PersonalSite/static/js/2.73d69356.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffbb9a88f03274954a85",
+    "url": "/PersonalSite/static/js/main.0b242e06.chunk.js"
+  },
+  {
+    "revision": "cd0a0012d82780645297",
+    "url": "/PersonalSite/static/js/runtime-main.b9187d47.js"
+  },
+  {
+    "revision": "6f0254a26e2895d52cc0861b8aeaef05",
+    "url": "/PersonalSite/static/media/profile.6f0254a2.jpg"
+  },
+  {
+    "revision": "a26213957a25a8881688413bb8a88dc7",
+    "url": "/PersonalSite/static/media/resume.a2621395.pdf"
+  }
+]);
